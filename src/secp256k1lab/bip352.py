44ecbf299290293e5a26b02ac64559fbcd59554e
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2025-11-06 20:30:32 +0100

    implement _recipient_scan_outputs without labels support

--- src/secp256k1lab/bip352.py
+++ src/secp256k1lab/bip352.py
@@ -14,11 +14,18 @@ class silentpayments_recipient(NamedTuple):
     index: int
 
 
+# Note that in the secp256k1 PR this data type is opaque, i.e.
+# from the API perspective these fields are not accessible
 class silentpayments_prevouts_summary(NamedTuple):
-    # Note that in the secp256k1 PR this data type is opaque, i.e.
-    # from the API perspective these fields are not accessible
     pubkey_sum: GE
     input_hash: Scalar
+
+
+class silentpayments_found_output(NamedTuple):
+    output: bytes  # serialized x-only pubkey
+    tweak: bytes
+    found_with_label: bool
+    label: GE
 
 
 def _create_input_hash(outpoint_smallest: bytes, pubkey_sum: GE) -> Scalar:
@@ -31,6 +38,12 @@ def _create_output_tweak(shared_secret: bytes, k: int) -> Scalar:
     assert len(shared_secret) == 33
     data_to_hash = shared_secret + k.to_bytes(4, 'big')
     return Scalar.from_bytes_checked(tagged_hash("BIP0352/SharedSecret", data_to_hash))
+
+
+def _create_output_pubkey(shared_secret: bytes, spend_pubkey: GE, k: int) -> bytes:
+    assert len(shared_secret) == 33
+    output_tweak = _create_output_tweak(shared_secret, k)
+    return (spend_pubkey + output_tweak * G).to_bytes_xonly()
 
 
 def silentpayments_sender_create_outputs(recipients: List[silentpayments_recipient], outpoint_smallest: bytes,
@@ -77,9 +90,8 @@ def silentpayments_sender_create_outputs(recipients: List[silentpayments_recipie
             # if we are on a different scan pubkey, its time to recreate the shared secret and reset k to 0
             shared_secret = (secret_component * recipients[i].scan_pubkey).to_bytes_compressed()
             k = 0
-        output_tweak = _create_output_tweak(shared_secret, k)
-        output = recipients[i].spend_pubkey + output_tweak * G
-        created_outputs[recipients[i].index] = output.to_bytes_xonly()
+        output_xonly = _create_output_pubkey(shared_secret, recipients[i].spend_pubkey, k)
+        created_outputs[recipients[i].index] = output_xonly
         k += 1
         current_scan_pubkey = recipients[i].scan_pubkey
 
@@ -118,13 +130,37 @@ def silentpayments_recipient_prevouts_summary_create(outpoint_smallest: bytes, x
 
 # to keep it simple, the label lookup is hardcoded here and not passed as parameter
 def silentpayments_recipient_scan_outputs(tx_outputs: List[bytes], scan_key: bytes,
-                                          prevouts_summary: silentpayments_prevouts_summary, unlabeled_spend_pubkey: GE):
+                                          prevouts_summary: silentpayments_prevouts_summary,
+                                          unlabeled_spend_pubkey: GE) -> List[silentpayments_found_output]:
     # calculate the shared secret
     secret_component = prevouts_summary.input_hash * Scalar.from_bytes_checked(scan_key)
     shared_secret = secret_component * unlabeled_spend_pubkey
 
+    # scan through all outputs starting with k = 0;
+    # if an output is found, repeat with k = 1, etc.
+    found_outputs = []
     found_idx = 0
     for k in range(0, len(tx_outputs)):
-        tx_output_ge = GE.from_bytes_xonly(tx_outputs[k])
         output_tweak = _create_output_tweak(shared_secret, k)
-    # TODO: finish
+        output_ge = unlabeled_spend_pubkey + (output_tweak * G)
+        output_xonly = output_ge.to_bytes_xonly()
+        found = False
+        for j in range(0, len(tx_outputs)):
+            # check for direct match (no labels involved)
+            if output_xonly == tx_outputs[j]:
+                found = True
+                found_idx = j
+                break
+
+            # TODO: implement label scanning
+
+        if found:
+            fo_output = tx_outputs[found_idx]
+            fo_tweak = output_tweak.to_bytes()
+            fo_found_with_label = False
+            fo_label = GE()  # invalid label
+            found_outputs.append(silentpayments_found_output( fo_output, fo_tweak, fo_found_with_label, fo_label))
+        else:
+            break
+
+    return found_outputs
